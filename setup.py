1dbfed7f6bf3ee2507e3a631facd2ac7666de188
pahntanapat <pahntanapat@gmail.com>
2022-03-01 16:51:56 +0700

    add numpy

--- setup.py
+++ setup.py
@@ -3,32 +3,14 @@ import glob
 import os.path
 import sys
 
-
 C_SRC_PREFIX = os.path.join('cbits', 'webrtc', 'common_audio')
 C_SRC_RTC_PREFIX = os.path.join('cbits', 'webrtc', 'rtc_base')
 
-c_sources = (
-    [os.path.join(
-        'cbits', 'pywebrtcvad.c')]
-    + glob.glob(
-        os.path.join(
-            C_SRC_PREFIX,
-            'signal_processing',
-            '*.c'))
-    + glob.glob(
-        os.path.join(
-            C_SRC_PREFIX,
-            'third_party',
-            '*.c'))
-    + glob.glob(
-        os.path.join(
-            C_SRC_PREFIX,
-            'vad',
-            '*.c'))
-    + glob.glob(
-        os.path.join(
-            C_SRC_RTC_PREFIX,
-            'checks.cc')))
+c_sources = ([os.path.join('cbits', 'pywebrtcvad.c')] + glob.glob(
+    os.path.join(C_SRC_PREFIX, 'signal_processing', '*.c')) +
+             glob.glob(os.path.join(C_SRC_PREFIX, 'third_party', '*.c')) +
+             glob.glob(os.path.join(C_SRC_PREFIX, 'vad', '*.c')) +
+             glob.glob(os.path.join(C_SRC_RTC_PREFIX, 'checks.cc')))
 
 define_macros = []
 extra_compile_args = []
@@ -36,7 +18,9 @@ extra_compile_args = []
 if sys.platform.startswith('win'):
     define_macros.extend([('_WIN32', None), ('WEBRTC_WIN', None)])
 else:
-    define_macros.extend([('WEBRTC_POSIX', None), ])
+    define_macros.extend([
+        ('WEBRTC_POSIX', None),
+    ])
     extra_compile_args.extend(['-std=c++11'])
 
 module = Extension('_webrtcvad',
@@ -52,14 +36,14 @@ with open(os.path.join(here, 'README.rst')) as f:
     long_description = f.read()
 
 setup(
-    name='webrtcvad',
-    author='John Wiseman',
-    author_email='jjwiseman@gmail.com',
+    name='chunkwebrtcvad',
+    author='Tanapat Kahabodeekanokkul',
+    author_email='pahntanapat@gmail.com',
     version='2.0.11.dev0',
     description=('Python interface to the Google WebRTC Voice '
                  'Activity Detector (VAD)'),
     long_description=long_description,
-    url='https://github.com/wiseman/py-webrtcvad',
+    url='https://github.com/pahntanapat/py-webrtcvad',
     license='MIT',
     classifiers=[
         # How mature is this project? Common values are
@@ -88,6 +72,9 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
     ],
     keywords='speechrecognition asr voiceactivitydetection vad webrtc',
     ext_modules=[module],
@@ -96,7 +83,10 @@ setup(
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example: $ pip install -e .[dev,test]
+    install_requires=['numpy'],
     extras_require={
-        'dev': ['nose', 'check-manifest', 'unittest2', 'zest.releaser',
-                'psutil', 'memory_profiler']
+        'dev': [
+            'nose', 'check-manifest', 'unittest2', 'zest.releaser', 'psutil',
+            'memory_profiler'
+        ]
     })
